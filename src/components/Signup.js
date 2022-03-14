dde6e4ee93c027bafb115d5ee1c5d286d16cca5a
Unni Krishnan <kvunni2002@gmail.com>
2022-03-14 19:42:58 +0530

    fix

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Singup = (props) => {
   const [credentials, setCredentials] = useState({ email: "", password: "", name:"", cpassword:"" });
